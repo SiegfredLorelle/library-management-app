1600a375bffe8abc9667e41305dba3b9f3b46431
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-08-21 14:52:06 +0800

    Create auth system in laravel from scratch

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Session;
+
+
+class AuthController extends Controller
+{
+    public function index() 
+    {
+        return view("auth.login");
+    }
+
+    public function registration() 
+    {
+        return view("auth.registration");
+    }
+
+    public function logout()
+    {
+        Session::flush();
+        Auth::logout();
+        return redirect("login");
+    }
+
+    public function postRegistration(Request $request)
+    {
+        // dd($request->all());
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required|min:6",
+        ]);
+        $data = $request->all();
+        $createUser = $this->create($data);
+        return redirect("login")->withSuccess("You are registered successfully!");
+    }
+
+    public function create(array $data)
+    {
+        return User::create([
+            "name" => $data["name"],
+            "email" => $data["email"],
+            "password" => $data["password"],
+        ]);
+    }
+
+    public function postLogin(Request $request)
+    {
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required",
+        ]);
+
+        $checkLoginCredentials = $request->only("email", "password");
+        if (Auth::attempt($checkLoginCredentials)) {
+            return redirect("dashboard")->withSuccess ("You are registered logged in!");
+        }
+        return redirect("login")->withSuccess("Login credentials are incorrect.");
+    }
+
+    public function dashboard()
+    {
+        if(Auth::check()) {
+            return view("dashboard");
+        }
+        else {
+            return redirect("login")->withSuccess("Login to access the dashboard page.");
+        }
+    }
+
+}
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Registration and Logout</title>
+
+    <!-- Import Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Bootstrap Example</title>
+	<meta charset="utf-8">
+	<meta name="viewport"
+		content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href=
+"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<script src=
+"https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+	</script>
+	<script src=
+"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js">
+	</script>
+</head>
+<body>
+<header class="p-3 bg-dark text-white">
+        <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+            </a>
+
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <li><a href="{{ url("/") }}" class="nav-link px-2 text-secondary">Home</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+            </ul>
+
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+            <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+            </form>
+
+            <div class="text-end">
+                @guest
+                    <a href="{{ url('login') }}"><button type="button" class="btn btn-outline-light me-2">Login</button></a>
+                    <a href="{{ url('registration') }}"><button type="button" class="btn btn-warning">Sign-up</button></a>
+                @else
+                <a href="{{ url('logout') }}"><button type="button" class="btn btn-outline-light me-2">Logout</button></a>
+                @endguest
+
+            </div>
+        </div>
+        </div>
+    </header>
+</body>
+</html>
+
+
+    @yield("content")
+
+
+</body>
+</html>
