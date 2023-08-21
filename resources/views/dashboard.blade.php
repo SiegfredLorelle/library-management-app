03d4f5022ece282f854c2418822b8de52a02fcb8
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-08-21 15:11:22 +0800

    Add indicator for dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,6 @@
 @extends("layout")
 @section("content")
-    DASHBAORD
+    <div class="container-fluid min-vh-100 d-flex flex-column justify-content-center">
+        <h1 class="text-center">Dashboard Here</h1>
+    </div>
 @endsection
