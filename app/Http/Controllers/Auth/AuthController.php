d1d010142dd79aa3685703671fee9549f27e0f65
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-08-28 14:55:53 +0800

    Upload imgs, use relative path for favicon

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -207,7 +207,7 @@ class AuthController extends Controller
 // TODO:
 // style table (center elements, add icons to edit n delete btn)
 // turn edit form into modal
+// add icon to add, edit, delete btn
 // add duck icon in header
 // fix highlight in nav
 // check if editing books/user changed smth
-// fix typo in lvl-3 admin select
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="https://icons.iconarchive.com/icons/thesquid.ink/free-flat-sample/256/rubber-duck-icon.png">
+    <link rel="icon" href="images/logo.png">
     <title>Duck Library App</title>
 
     <!-- Import Bootstrap -->
