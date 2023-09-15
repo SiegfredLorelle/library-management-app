c8c80fd3a13a5089e86e77232d008e5bec4ccf14
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-09-15 11:48:35 +0800

    Render time before deadline on pending transaction

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -28,11 +28,9 @@
     @endif
     <div class="container-fluid d-flex flex-column mt-5">
 
-
         <table class="table table-hover">
             <thead>
                 <tr>
-                    <th scope="col">#</th>
                     <th scope="col">Borrowed by</th>
                     <th scope="col">Title</th>
                     <th scope="col">Author</th>
@@ -42,11 +40,10 @@
             </thead>
             <tbody class="table-group-divider">
                 @if (auth()->user()->user_level == "lvl-3")
-                
+
                 @else
-                    @foreach ($borrowedBooks as $i => $borrowedBook)
+                    @foreach ($borrowedBooks as $borrowedBook)
                         <tr>
-                            <td>{{ $i + 1 }}</td>
                             @foreach ($books as $book)
                                 @if ($book->id == $borrowedBook->book_id)
                                     <td>
@@ -59,8 +56,8 @@
                                     <td>{{ $book->title }}</td>
                                     <td>{{ $book->author }}</td>
                                     <td>{{ $borrowedBook->deadline }}</td>
-                                    <td>{{ $borrowedBook->deadline }}</td>
-                                @break
+                                    <td>{{ \Carbon\Carbon::parse($borrowedBook->deadline)->diffForHumans() }}</td>                                    {{-- <td>{{ new DateTime($borrowedBook->deadline)->diff($borrowedBook->deadline) }}</td> --}}
+                                    @break
                                 @endif
                             @endforeach
                         </tr>
